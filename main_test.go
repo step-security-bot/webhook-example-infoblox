273948831634c88801547dda6c8f4906d1389e85
fadecore <outbreak86@hotmail.com>
2023-11-12 16:33:00 +0100

    Update main_test.go
    Reset to original
    
    Signed-off-by: fadecore <outbreak86@hotmail.com>

--- main_test.go
+++ main_test.go
@@ -5,49 +5,37 @@ import (
 	"testing"
 
 	"github.com/cert-manager/cert-manager/test/acme/dns"
+
+	"github.com/cert-manager/webhook-example/example"
 )
 
 var (
 	zone = os.Getenv("TEST_ZONE_NAME")
 )
 
-//	func TestRunsSuite(t *testing.T) {
-//		// The manifest path should contain a file named config.json that is a
-//		// snippet of valid configuration that should be included on the
-//		// ChallengeRequest passed as part of the test cases.
-//		//
-//
-//		// Uncomment the below fixture when implementing your custom DNS provider
-//		//fixture := dns.NewFixture(&customDNSProviderSolver{},
-//		//	dns.SetResolvedZone(zone),
-//		//	dns.SetAllowAmbientCredentials(false),
-//		//	dns.SetManifestPath("testdata/my-custom-solver"),
-//		//	dns.SetBinariesPath("_test/kubebuilder/bin"),
-//		//)
-//		solver := example.New("59351")
-//		fixture := dns.NewFixture(solver,
-//			dns.SetResolvedZone("example.com."),
-//			dns.SetManifestPath("testdata/my-custom-solver"),
-//			dns.SetDNSServer("127.0.0.1:59351"),
-//			dns.SetUseAuthoritative(false),
-//		)
-//		//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
-//		//fixture.RunConformance(t)
-//		fixture.RunBasic(t)
-//		fixture.RunExtended(t)
-//
-// }
 func TestRunsSuite(t *testing.T) {
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
 	//
 
-	fixture := dns.NewFixture(&customDNSProviderSolver{},
-		dns.SetResolvedZone(zone),
-		dns.SetAllowAmbientCredentials(false),
-		dns.SetManifestPath("testdata/infoblox-wapi"),
+	// Uncomment the below fixture when implementing your custom DNS provider
+	//fixture := dns.NewFixture(&customDNSProviderSolver{},
+	//	dns.SetResolvedZone(zone),
+	//	dns.SetAllowAmbientCredentials(false),
+	//	dns.SetManifestPath("testdata/my-custom-solver"),
+	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
+	//)
+	solver := example.New("59351")
+	fixture := dns.NewFixture(solver,
+		dns.SetResolvedZone("example.com."),
+		dns.SetManifestPath("testdata/my-custom-solver"),
+		dns.SetDNSServer("127.0.0.1:59351"),
+		dns.SetUseAuthoritative(false),
 	)
+	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
+	//fixture.RunConformance(t)
+	fixture.RunBasic(t)
+	fixture.RunExtended(t)
 
-	fixture.RunConformance(t)
 }
